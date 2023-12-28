5eea19bf75bad803a2d2364e24119ec3579508ff
Christophe <negeko-inscriptions@hotmail.com>
2023-12-28 16:00:07 +0100

    Using Vue.js

--- index.ts
+++ index.ts
@@ -1,27 +1,55 @@
+// import {createApp, ref} from 'vue/dist/vue.esm-browser.js'
+import {createApp, ref, reactive} from 'vue';
+
 import { 
     loadXmlInput,
     loadRemoteXmlFile
  } from "./modules/XmlLoader.js";
-import { Field } from "./modules/fixClass/Field.js";
 
-var FIXT11 = "";
+import { FIXTree } from "./modules/fixClass/FIXTree.js";
 
-function load()
-{
-    loadXmlInput().then(console.log);
+declare global {
+    interface Window { 
+        fixTree: FIXTree; 
+        load: any;
+        serialize: any;
+    }
 }
 
 function init()
 {
     loadRemoteXmlFile("FIXML_templates/FIXT11.xml").then((res) => {
-        // FIXT11 = res;
         console.log(res);
-        // @ts-ignore
-        window.xmldoc = res;
     });
 }
 
-//@ts-ignore
-window.load = load;
+// const a = reactive(new FIXTree());
+
+window.fixTree = reactive(new FIXTree()) as FIXTree;
+console.log(window.fixTree._messagesMap);
+console.log(window.fixTree._fieldsMap);
+
+window.load = function() {
+    loadXmlInput().then(console.log);
+}
+
+window.serialize = () => {
+    return window.fixTree.serialize(true);
+}
 
 init();
+
+createApp({
+    setup() {
+        // const type = ref(window.fixTree._type);
+        // watch(fixTree._messagesMap)
+        // console.log(fixTree._messagesMap.size);
+        // watch(fixTree._messagesMap)
+        return {
+            tree: window.fixTree,
+            type: window.fixTree._type,
+            messages: window.fixTree._messagesMap,
+            fields: window.fixTree._fieldsMap,
+        }
+    }
+}).mount('#app')
